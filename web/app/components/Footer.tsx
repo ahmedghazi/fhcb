f612c5b355900966f06a4282108d9b8c9af13cba
ahmedghazi <atmet.ghazi@gmail.com>
2026-06-19 16:23:16 +0200

    footer container

--- web/app/components/Footer.tsx
+++ web/app/components/Footer.tsx
@@ -39,99 +39,104 @@ const Footer = ({ settings }: Props) => {
 
   return (
     <footer className='footer'>
-      <div className='footer__inner'>
-        <div className='footer__logo'>
-          <Link href='/'>
-            <LogoFHCB type='long' />
-          </Link>
-        </div>
-        <div className='navs'></div>
-        <div className='flex gap-gutter- items-baseline-'>
-          <address
-            style={{
-              width: `${logoTypeWidth}px`,
-            }}>
-            <div className='text'>
-              <PortableText
-                value={_localizeField(settings.adressAndOpeningHours)}
-              />
-            </div>
-          </address>
-          {settings?.navSecondary && (
-            <nav className='footer__nav'>
-              <div className='grid md:grid-cols-3 gap-gutter'>
-                <ul className='md:col-span-3 grid md:grid-cols-3 gap-gutter'>
-                  {settings.navSecondary.map((item: any) => {
-                    return (
-                      <li key={item._key}>
-                        <h5 className='title c-h2'>
-                          {_localizeField(item.label)}
-                        </h5>
-                        <ul className='sub-menu'>
-                          {item.subMenu?.map((subItem: any) => (
-                            <li key={subItem._key}>
-                              <Link href={_linkResolver(subItem.link)}>
-                                {_localizeField(subItem.label)}
+      <div className='container-fluid'>
+        <div className='footer__inner'>
+          <div className='footer__logo'>
+            <Link href='/'>
+              <LogoFHCB type='long' />
+            </Link>
+          </div>
+          <div className='navs'></div>
+          <div className='flex gap-gutter- items-baseline-'>
+            <address
+              style={{
+                width: `${logoTypeWidth}px`,
+              }}>
+              <div className='text'>
+                <PortableText
+                  value={_localizeField(settings.adressAndOpeningHours)}
+                />
+              </div>
+            </address>
+            {settings?.navSecondary && (
+              <nav className='footer__nav'>
+                <div className='grid md:grid-cols-3 gap-gutter'>
+                  <ul className='md:col-span-3 grid md:grid-cols-3 gap-gutter'>
+                    {settings.navSecondary.map((item: any) => {
+                      return (
+                        <li key={item._key}>
+                          <h5 className='title c-h2'>
+                            {_localizeField(item.label)}
+                          </h5>
+                          <ul className='sub-menu'>
+                            {item.subMenu?.map((subItem: any) => (
+                              <li key={subItem._key}>
+                                <Link href={_linkResolver(subItem.link)}>
+                                  {_localizeField(subItem.label)}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      );
+                    })}
+                    <li>
+                      <h5 className='c-h2'>{_localizeText("contactTeam")}</h5>
+                      {settings.navTertiary && (
+                        <ul>
+                          {settings.navTertiary?.map((item: any) => (
+                            <li key={item._key}>
+                              <Link href={_linkResolver(item.link)}>
+                                {_localizeField(item.label)}
                               </Link>
                             </li>
                           ))}
                         </ul>
-                      </li>
-                    );
-                  })}
-                  <li>
-                    <h5 className='c-h2'>{_localizeText("contactTeam")}</h5>
-                    {settings.navTertiary && (
-                      <ul>
-                        {settings.navTertiary?.map((item: any) => (
-                          <li key={item._key}>
-                            <Link href={_linkResolver(item.link)}>
-                              {_localizeField(item.label)}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                </ul>
-                <div className='credits-n-legals md:col-span-2'>
-                  © {new Date().getFullYear()} {website.title}{" "}
-                  {_localizeText("allRightsReserved")}
-                  {"  "}
-                  {settings.navLegals
-                    ?.map((item: any) => (
-                      <Link key={item._key} href={_linkResolver(item.link)}>
-                        {_localizeField(item.label)}
-                      </Link>
-                    ))
-                    .reduce((acc: any, curr: any) => [...acc, curr, ",  "], [])
-                    .slice(0, -1)}
-                </div>
-                <ul className='nav-actions flex gap-2xs items-center'>
-                  <li>
-                    <button className='btn btn--accent'>Nous soutenir</button>
-                  </li>
-                  <li>
-                    <button className='btn btn--accent'>Newsletter</button>
-                  </li>
-                  {settings.navSocial?.map((item) => (
-                    <li key={item.label}>
-                      <Link href={_linkResolver(item.link)}>
-                        {item.icon?.asset?.url && (
-                          <Image
-                            src={item.icon.asset.url}
-                            width={28}
-                            height={28}
-                            alt={item.label || ""}
-                          />
-                        )}
-                      </Link>
+                      )}
                     </li>
-                  ))}
-                </ul>
-              </div>
-            </nav>
-          )}
+                  </ul>
+                  <div className='credits-n-legals md:col-span-2'>
+                    © {new Date().getFullYear()} {website.title}{" "}
+                    {_localizeText("allRightsReserved")}
+                    {"  "}
+                    {settings.navLegals
+                      ?.map((item: any) => (
+                        <Link key={item._key} href={_linkResolver(item.link)}>
+                          {_localizeField(item.label)}
+                        </Link>
+                      ))
+                      .reduce(
+                        (acc: any, curr: any) => [...acc, curr, ",  "],
+                        [],
+                      )
+                      .slice(0, -1)}
+                  </div>
+                  <ul className='nav-actions flex gap-2xs items-center'>
+                    <li>
+                      <button className='btn btn--accent'>Nous soutenir</button>
+                    </li>
+                    <li>
+                      <button className='btn btn--accent'>Newsletter</button>
+                    </li>
+                    {settings.navSocial?.map((item) => (
+                      <li key={item.label}>
+                        <Link href={_linkResolver(item.link)}>
+                          {item.icon?.asset?.url && (
+                            <Image
+                              src={item.icon.asset.url}
+                              width={28}
+                              height={28}
+                              alt={item.label || ""}
+                            />
+                          )}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </nav>
+            )}
+          </div>
         </div>
       </div>
     </footer>
