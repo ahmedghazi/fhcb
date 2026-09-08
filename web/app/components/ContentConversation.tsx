64e10ce4d6b6e0d88da25ef4e7a13cc90c2015d4
ahmedghazi <atmet.ghazi@gmail.com>
2026-09-08 11:37:06 +0200

    fix mobile

--- web/app/components/ContentConversation.tsx
+++ web/app/components/ContentConversation.tsx
@@ -24,7 +24,7 @@ const ContentConversation = ({ input }: Props) => {
         )}
 
         {input?.text && (
-          <div className='grid md:grid-cols-4 gap-gutter'>
+          <div className='grid grid-cols-1 md:grid-cols-4 gap-gutter'>
             <div className='md:col-span-3-'></div>
             <div className='md:col-span-2'>
               <div className='text'>
